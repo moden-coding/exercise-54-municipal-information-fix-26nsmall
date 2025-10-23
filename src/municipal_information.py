b79a8d4f9a063bf2cf5523beb9da8882f758fdb5
26nsmall <26nsmall@gannacademy.org>
2025-10-23 12:01:27 -0400

    commit

--- src/municipal_information.py
+++ src/municipal_information.py
@@ -4,7 +4,13 @@ from os import sep
 import pandas as pd
 
 def main():
-    pass 
+    data =pd.read_csv("src\municipal.tsv", sep="\t")
+    print(f"Shape: {data.shape[0]},{data.shape[1]}")
+    print("Columns:")
+    names = data.columns
+    for name in names:
+        print(name)
+    
 
 
 if __name__ == "__main__":
